af769b8db5fa0975860e30787e3e1dcb9735eb3d
Sahanshil Thapa <94502748+sunsilthapa@users.noreply.github.com>
2024-07-26 14:47:51 +0545

    XSRF implementation

--- backend/middleware/csrfProtection.js
+++ backend/middleware/csrfProtection.js
@@ -1,5 +1,14 @@
+// middleware/csrfProtection.js
 const csrf = require('csurf');
 
 const csrfProtection = csrf({ cookie: true });
 
-module.exports = csrfProtection;
+const csrfProtectionMiddleware = (req, res, next) => {
+    csrfProtection(req, res, () => {
+        const token = req.csrfToken(); // Generate or access the CSRF token
+        console.log('CSRF Token:', token); // Log the token for debugging
+        next();
+    });
+};
+
+module.exports = csrfProtectionMiddleware;
